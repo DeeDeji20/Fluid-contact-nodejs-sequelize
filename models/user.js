e48d82fd5002c8eecd63f0393279ab6a5fa7f97e
Adeola Oladeji <deolaoladeji@gmail.com>
2022-05-20 14:41:55 +0100

    fluid contact project

--- models/user.js
+++ models/user.js
@@ -9,36 +9,48 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Contact}) {
       // define association here
+      this.hasMany(Contact, {foreignKey:'userId', as: 'contact'})
     }
 
     toJSON(){
-      return {...this.get(), id: undefined}
+      return { ...this.get(), undefined }
     }
   }
-
- 
   User.init({
     uuid: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
     },
-    username: {
+    userName: {
       type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notNull:{ msg: 'Username can not be blank' },
+        notEmpty: {msg:'Username must not be empty'},
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notNull:{ msg: 'Email can not be blank' },
+        notEmpty: {msg:'Email must not be empty'},
+        isEmail: { msg: 'Must be a valid email address' }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notNull:{ msg: 'Password can not be blank' },
+        notEmpty: {msg: 'Password muct not be empty'}
+      }
     },
   }, {
     sequelize,
-    tableName: 'users',
+    tableName: 'user',
     modelName: 'User',
   });
   return User;
